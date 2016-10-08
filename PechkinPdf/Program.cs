cd23c9f374664cfd31c5ed265ae95d80518e0ba0
vsol <mahadevan.balasubramanian@bunzl.co.uk>
2016-10-08 23:55:18 +0100

    solstransform added

--- PechkinPdf/Program.cs
+++ PechkinPdf/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Runtime.InteropServices;
 
 namespace PechkinPdf
 {
@@ -6,9 +7,11 @@ namespace PechkinPdf
     {
         static void Main(string[] args)
         {
-            var html = File.ReadAllText(@"C:\Users\maha8\Documents\visual studio 2015\Projects\PechkinPdf\PechkinPdf\SOLSDocumentCopy.html");
-            var pdf = DocumentHelper.CreatePdfFromHtml(html);
-            File.WriteAllBytes(@"C:\\tmp\pechkinpdfoutput.pdf", pdf);
+            //var html = File.ReadAllText(@"C:\Users\maha8\Documents\visual studio 2015\Projects\PechkinPdf\PechkinPdf\SOLSDocumentCopy.html");
+            var xmlString = File.ReadAllText(@"C:\Users\maha8\Documents\visual studio 2015\Projects\PechkinPdf\PechkinPdf\Sols.xml");
+            var transformedOutput = DocumentHelper.TransformToHtml(xmlString, @"C:\Users\maha8\Documents\visual studio 2015\Projects\PechkinPdf\PechkinPdf\SwitchRedirection.xslt");
+            var pdf = DocumentHelper.CreatePdfFromHtml(transformedOutput);
+            File.WriteAllBytes(@"C:\\tmp\transformedpechkinpdf.pdf", pdf);
         }
     }
 }
